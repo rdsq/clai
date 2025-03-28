3d79028294fd29978b4b688fefde269d91237eec
rdsq <62446963+rdsq@users.noreply.github.com>
2025-03-28 22:31:49 +0200

    remove an unwrap

--- src/get_interface.rs
+++ src/get_interface.rs
@@ -6,6 +6,9 @@ pub fn get_interface(input: &str) -> Box<dyn crate::interfaces::frame::Interface
         eprintln!("{}", err.to_string());
         exit(1);
     });
-    let interface = crate::interfaces::get_interface(&parsed.interface, parsed.model.to_string()).unwrap();
+    let interface = crate::interfaces::get_interface(&parsed.interface, parsed.model.to_string()).unwrap_or_else(|err| {
+        eprintln!("{}", err.to_string());
+        exit(1);
+    });
     interface
 }
