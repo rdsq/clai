17b198ba453155259197afb8833bf1e8db5f36f8
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-10 18:07:16 +0000

    chat saving

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -7,10 +7,19 @@ pub struct Generate {
     model: String,
     /// The user message
     prompt: String,
+    /// Read and save the chat to a file
+    #[arg(short, long, default_value = None)]
+    file: Option<String>,
 }
 
 pub async fn generate(args: Generate) {
-    let context = ContextState::new();
+    let context = match &args.file {
+        Some(path) => ContextState::from_file(&path, true),
+        None => ContextState::new()
+    };
     let mut state = AppState::new(context, &args.model);
     state.generate_to_output(args.prompt).await;
+    if let Some(path) = args.file {
+        state.context.write_to_file(&path);
+    }
 }
--- src/states/context.rs
+++ src/states/context.rs
@@ -1,4 +1,7 @@
 use serde::{Serialize, Deserialize};
+use std::fs;
+use serde_json;
+use std::io::Write;
 
 #[derive(Serialize, Deserialize)]
 pub enum Role {
@@ -20,6 +23,41 @@ pub struct ContextState {
 impl ContextState {
     pub fn new() -> Self {
         Self { chat: Vec::new() }
+    }
+    pub fn from_file(path: &str, allow_not_existing: bool) -> Self {
+        let contents = fs::read_to_string(path);
+        if let Err(err) = contents {
+            if allow_not_existing && err.kind() == std::io::ErrorKind::NotFound {
+                return Self::new();
+            } else {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        }
+        let contents = contents.unwrap();
+        return serde_json::from_str(&contents)
+            .unwrap_or_else(|e| {
+                eprintln!("JSON parsing error: {}", e);
+                std::process::exit(1);
+            });
+    }
+    pub fn write_to_file(&self, path: &str) {
+        let contents = serde_json::to_string_pretty(&self)
+            .unwrap_or_else(|e| {
+                eprintln!("JSON stringifying error: {}", e);
+                std::process::exit(1);
+            });
+        let mut file = fs::File::create(path)
+            .unwrap_or_else(|e| {
+                eprintln!("Error while creating a file: {}", e);
+                // will it cause issues with overwriting? We'll find out
+                std::process::exit(1);
+            });
+        file.write_all(contents.as_bytes())
+            .unwrap_or_else(|e| {
+                eprintln!("Error while writing the file: {}", e);
+                std::process::exit(1);
+            });
     }
 }
 
