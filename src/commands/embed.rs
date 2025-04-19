fd5a17bc1638f71ebaf1ccaf77710b4d3884278c
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-19 14:34:13 +0000

    io formats yay

--- src/commands/embed.rs
+++ src/commands/embed.rs
@@ -1,5 +1,17 @@
 use crate::states::InterfaceState;
 
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, clap::ValueEnum, Debug)]
+enum IOFormats {
+    Plain,
+    JSON,
+}
+
+#[derive(serde::Serialize)]
+struct OutputObject<'a> {
+    embedding: &'a Vec<f32>,
+    text: &'a str,
+}
+
 #[derive(clap::Parser, Debug)]
 /// Embed strings and output their embedding values. Can be used for caching
 pub struct Embed {
@@ -7,12 +19,15 @@ pub struct Embed {
     model: String,
     /// The strings to embed
     items: Vec<String>,
-    /// Output the original strings too
+    /// Output the original strings too (in plain output format)
     #[arg(short, long)]
     pairs: bool,
-    /// Get the items from a JSON array of strings if shell is too much pain, from first argument
-    #[arg(short, long)]
-    json: bool,
+    /// Format for the input items
+    #[arg(short, long, default_value = "plain")]
+    input_format: IOFormats,
+    /// Format for the result items
+    #[arg(short, long, default_value = "plain")]
+    output_format: IOFormats,
 }
 
 fn create_label<'a>(original: &str) -> String {
@@ -25,30 +40,46 @@ fn create_label<'a>(original: &str) -> String {
 
 pub async fn embed(args: Embed) {
     let state = InterfaceState::new(&args.model);
-    let items: Vec<String> = if args.json {
-        if args.items.is_empty() {
-            eprintln!("Provide an argument with a JSON array");
-            std::process::exit(1);
-        }
-        serde_json::from_str(&args.items[0])
-            .unwrap_or_else(|err| {
-                eprintln!("Error while parsing JSON: {}", err);
+    let items: Vec<String> = match args.input_format {
+        IOFormats::Plain => args.items,
+        IOFormats::JSON => {
+            if args.items.is_empty() {
+                eprintln!("Provide an argument with a JSON array");
                 std::process::exit(1);
-            })
-    } else {
-        args.items
+            }
+            serde_json::from_str(&args.items[0])
+                .unwrap_or_else(|err| {
+                    eprintln!("Error while parsing JSON: {}", err);
+                    std::process::exit(1);
+                })
+        }
     };
     let embeddings = state.interface.embeddings(&items).await
         .unwrap_or_else(|err| {
             eprintln!("{}", err);
             std::process::exit(1);
         });
-    for (i, emb) in embeddings.into_iter().enumerate() {
-        print!("{}", emb.iter().map(|v| v.to_string()).collect::<Vec<String>>().join(","));
-        if args.pairs {
-            println!(" {}", create_label(&items[i]));
-        } else {
-            println!();
-        }
-    }
+    let pairs = embeddings.iter()
+        .zip(items.iter());
+    match args.output_format {
+        IOFormats::Plain => for (emb, label) in pairs {
+            print!("{}", emb.iter().map(|v| v.to_string()).collect::<Vec<String>>().join(","));
+            if args.pairs {
+                println!(" {}", create_label(&label));
+            } else {
+                println!();
+            }
+        },
+        IOFormats::JSON => {
+            let stringified = serde_json::to_string(
+                &pairs
+                .map(|(embedding, text)| OutputObject {
+                    embedding,
+                    text,
+                })
+                .collect::<Vec<OutputObject>>()
+            ).unwrap();
+            println!("{}", stringified);
+        },
+    };
 }
--- src/commands/semantic_search.rs
+++ src/commands/semantic_search.rs
@@ -12,15 +12,26 @@ pub struct SemanticSearch {
     /// How many top results to show
     #[arg(short, long, default_value = "10")]
     show: usize,
-    /// Treat the items as file paths
-    #[arg(short, long)]
-    files: bool,
     /// Show the similarity numbers
     #[arg(short, long)]
     verbose: bool,
-    /// Parse every argument as a pre embedded string
-    #[arg(short, long)]
-    pre: bool,
+    /// Format of the input intems
+    #[arg(short = 'f', long, default_value = "text")]
+    input_format: InputFormats,
+}
+
+#[derive(serde::Deserialize)]
+struct InputObject {
+    embedding: Vec<f32>,
+    text: String,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, clap::ValueEnum, Debug)]
+enum InputFormats {
+    Text,
+    Pre,
+    JSON,
+    File,
 }
 
 fn cosine_similarity(a: &[f32], b: &[f32]) -> f32 {
@@ -40,49 +51,69 @@ fn parse_pre(item: &str) -> Result<(String, Vec<f32>), Box<dyn std::error::Error
     Ok((name.to_string(), parsed))
 }
 
-async fn get_embeddings(state: &InterfaceState, items: &Vec<String>, files: &bool, pre: &bool) -> (Vec<String>, Vec<Vec<f32>>) {
+async fn get_embeddings(state: &InterfaceState, items: &Vec<String>, input_format: InputFormats) -> (Vec<String>, Vec<Vec<f32>>) {
     let mut names_vec = Vec::new();
     let mut embeds_vec = Vec::new();
-    if *pre {
-        for (i, item) in items.iter().enumerate() {
-            let (name, parsed) = parse_pre(&item)
+    match input_format {
+        InputFormats::Pre => {
+            for (i, item) in items.iter().enumerate() {
+                let (name, parsed) = parse_pre(&item)
+                    .unwrap_or_else(|err| {
+                        eprintln!("Error while parsing item {}: {}", i+1, err);
+                        std::process::exit(1);
+                    });
+                names_vec.push(name);
+                embeds_vec.push(parsed);
+            }
+        },
+        InputFormats::File => {
+            let mut values = Vec::new();
+            for path in items {
+                values.push(std::fs::read_to_string(&path).unwrap_or_else(|err| {
+                    eprintln!("Error while reading \"{}\": {}", path, err);
+                    std::process::exit(1);
+                }));
+            }
+            names_vec.extend(items.clone());
+            let embeddings = state.interface.embeddings(&values).await
                 .unwrap_or_else(|err| {
-                    eprintln!("Error while parsing item {}: {}", i+1, err);
+                    eprintln!("{}", err);
                     std::process::exit(1);
                 });
-            names_vec.push(name);
-            embeds_vec.push(parsed);
-        }
-    } else if *files {
-        let mut values = Vec::new();
-        for path in items {
-            values.push(std::fs::read_to_string(&path).unwrap_or_else(|err| {
-                eprintln!("Error while reading \"{}\": {}", path, err);
-                std::process::exit(1);
-            }));
-        }
-        names_vec.extend(items.clone());
-        let embeddings = state.interface.embeddings(&values).await
-            .unwrap_or_else(|err| {
-                eprintln!("{}", err);
-                std::process::exit(1);
-            });
-        embeds_vec.extend(embeddings);
-    } else {
-        let embeddings = state.interface.embeddings(&items).await
-            .unwrap_or_else(|err| {
-                eprintln!("{}", err);
-                std::process::exit(1);
-            });
-        embeds_vec.extend(embeddings);
-        names_vec.extend(items.clone());
+            embeds_vec.extend(embeddings);
+        },
+        InputFormats::Text => {
+            let embeddings = state.interface.embeddings(&items).await
+                .unwrap_or_else(|err| {
+                    eprintln!("{}", err);
+                    std::process::exit(1);
+                });
+            embeds_vec.extend(embeddings);
+            names_vec.extend(items.clone());
+        },
+        InputFormats::JSON => {
+            let arg = &items.iter().nth(0)
+                .unwrap_or_else(|| {
+                    eprintln!("Provide an argument with a JSON array");
+                    std::process::exit(1);
+                });
+            let arr: Vec<InputObject> = serde_json::from_str(arg)
+                .unwrap_or_else(|err| {
+                    eprintln!("Error while parsing JSON: {}", err);
+                    std::process::exit(1);
+                });
+            for i in arr {
+                names_vec.push(i.text);
+                embeds_vec.push(i.embedding);
+            }
+        },
     }
     (names_vec, embeds_vec)
 }
 
 pub async fn semantic_search(args: SemanticSearch) {
     let state = InterfaceState::new(&args.model);
-    let (names, embeds) = get_embeddings(&state, &args.items, &args.files, &args.pre).await;
+    let (names, embeds) = get_embeddings(&state, &args.items, args.input_format).await;
     let prompt_score = &state.interface.embeddings(&vec![args.prompt]).await
         .unwrap_or_else(|err| {
             eprintln!("Error while embedding prompt: {}", err);
