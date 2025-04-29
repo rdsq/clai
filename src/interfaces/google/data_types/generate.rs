1c9e6c3eef206e5b7ea0f8f249590f7866169803
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-29 20:33:27 +0000

    and a bit more

--- src/interfaces/google/data_types/generate.rs
+++ src/interfaces/google/data_types/generate.rs
@@ -5,19 +5,19 @@ use crate::states::messages;
 #[derive(Serialize)]
 pub struct GoogleGenAIMessage<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub role: Option<String>,
+    pub role: Option<&'a str>,
     pub parts: Vec<MessageParts<'a>>,
 }
 
 impl<'a> GoogleGenAIMessage<'a> {
-    pub fn new(role: Option<String>, text: &'a str) -> Self {
+    pub fn new(role: Option<&'a str>, text: &'a str) -> Self {
         Self { role, parts: vec![MessageParts { text }] }
     }
     pub fn from(message: &'a messages::Message) -> Self {
         Self::new(
             Some(match message.role {
-                messages::Role::User => "user".to_string(),
-                messages::Role::Model => "model".to_string(),
+                messages::Role::User => "user",
+                messages::Role::Model => "model",
             }),
             &message.text,
         )
