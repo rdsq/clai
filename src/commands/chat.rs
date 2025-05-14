0fe789ad81c56e44fa1c035b8b70238b54af8f80
rdsq <62446963+rdsq@users.noreply.github.com>
2025-05-14 16:48:43 +0000

    no exit

--- src/commands/chat.rs
+++ src/commands/chat.rs
@@ -90,7 +90,6 @@ pub async fn chat(args: Chat) {
                 UserActions::Save(path) => state.context.write_to_file(&path)
                     .unwrap_or_else(|err| {
                         eprintln!("{}", err);
-                        std::process::exit(1);
                     }),
                 UserActions::SetFile(path) => state.autosave = if path.is_empty() { None } else { Some(path) },
                 UserActions::Help => print!(include_str!("../help-interactive.txt")),
